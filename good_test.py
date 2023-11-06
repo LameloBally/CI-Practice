02a1faf8a2bd0426da0fa884e307e25b846bee08
WoosungMyung <dntjd517@naver.com>
2023-11-06 17:49:19 +0900

    unittest check

--- good_test.py
+++ good_test.py
@@ -6,7 +6,7 @@ import good
 class helloTest(unittest.TestCase):
     def test_hello(self):
         ret = good.Hello("Myung")
-        self.assertEqual(ret, "Hello Nice to mee u, Myung")
+        self.assertEqual(ret, "Hello Nice to mee u, Maung")
 
 
 if __name__ == "__main__":
